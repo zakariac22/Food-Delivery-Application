ff0b6f81853890b8dfdd7086ccef61e2475236b5
Zakaria Chowdhury <zakariachowdhury@zakarias-air.mynetworksettings.com>
2023-12-23 01:41:04 -0500

    Commit

---
+++ application/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Home from './screens/Home';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Home/>
+      </div>
+    </>
+  );
+}
+
+export default App;
